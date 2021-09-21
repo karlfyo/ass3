5ef00bcafc871cb809a8f17e64821729f0184c64
karlfyo <karlfrotmg@gmail.com>
2021-09-21 18:45:59 +0200

    added stuff pt 1

--- main.cpp
+++ main.cpp
@@ -1,48 +1,90 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <string>
+using namespace sf;
 
+struct Node {
+
+    int x;
+    int y;
+    Node *next;
+};
 
 int main()
 {
+
+    Node *head = NULL;
+    Node *temp;
+    int mouse_x = 0;
+    int mouse_y = 0;
+    bool pressed = false;
+    bool started_drawing = false;
+
+
     // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    RenderWindow window(VideoMode(800, 600), "My window");
 
     // run the program as long as the window is open
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
+        Event event;
         while (window.pollEvent(event))
         {
             // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
 
         // clear the window with black color
-        window.clear(sf::Color::Black);
+        window.clear(Color::Black);
+        mouse_x = Mouse::getPosition(window).x;
+        mouse_y = Mouse::getPosition(window).y;
+        if (Mouse::isButtonPressed(Mouse::Left) && !pressed){
 
-        if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+            pressed = true;
 
 
-            int mousePOS_X = sf::Mouse::getPosition(window).x;
-            int mousePOS_Y = sf::Mouse::getPosition(window).y;
+            if(!started_drawing) {
+                started_drawing = true;
+            }
+
+            temp = new Node{mouse_x, mouse_y, head};
+            head = temp;
+            std::cout << temp->next << " / " << head << std::endl;
 
 
+        } else if(!Mouse::isButtonPressed(Mouse::Left)) pressed = false;
+        
+        if(started_drawing) {
+
+            Node *iteration = head;
+            Node *prev;
+            while (iteration != NULL) {
+                
+                Vertex line[2];
+                if(iteration == head) {
+
+
+                    line[0] = Vertex(Vector2f(mouse_x, mouse_y));
+                    line[1] = Vertex(Vector2f(iteration->x, iteration->y));
+
+                } else {
+
+
+                    line[0] = Vertex(Vector2f(prev->x, prev->y));
+                    line[1] = Vertex(Vector2f(iteration->x, iteration->y));
+
+                }
+                //std::cout << iteration->next << " / " << iteration << std::endl;
+                // draw everything here...
+                window.draw(line, 2, Lines);
+                prev = iteration;
+                iteration = iteration->next;
+            }
         }
 
-        sf::Vertex line[] =
-                {
-
-                        sf::Vertex(sf::Vector2f(1,10)),
-                        sf::Vertex(sf::Vector2f(150, 150))
-                };
-
-
-        // draw everything here...
-        window.draw(line,2, sf::Lines);
 
         // end the current frame
         window.display();
